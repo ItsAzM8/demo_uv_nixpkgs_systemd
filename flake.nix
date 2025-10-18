fee85a0602f29689be4c2d0e7fc9ef9531c6ff8a
Darcy Owens <owens.darcy67@gmail.com>
2025-10-18 14:36:51 +1030

    testing some things.

--- flake.nix
+++ flake.nix
@@ -9,44 +9,81 @@
     pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pyproject-nix, uv2nix
-    , pyproject-build-systems, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        systemdService = import ./nix/systemd-service.nix;
-        demoUvNixpkgsSystemd =
-          pkgs.callPackage ./nix/uv_nixpkgs_derivation.nix {
-            inherit pyproject-nix uv2nix pyproject-build-systems;
-          };
+        python = pkgs.python312;
 
-      in {
-        # Configuration for the systemd service.
-        nixosModules.uv_nixpkgs = systemdService;
-        nixosModules.default = systemdService;
-
-        # The built application for this Flake.
-        # What gets build with `nix flake build`.
-        packages.default = demoUvNixpkgsSystemd;
-
-        # Registering as a package within the
-        # `packages` attribute set.
-        packages.${demoUvNixpkgsSystemd.pname} =
-          self.packages.${system}.default;
-
-        # The configuration for what gets run when
-        # using `nix flake run`.
-        apps.default = {
-          type = "app";
-          program = "${
-              self.packages.${system}.default
-            }/bin/${demoUvNixpkgsSystemd.pname}";
+        demoUvNixpkgsDerivation = pkgs.callPackage ./nix/uv_nixpkgs_derivation.nix {
+          inherit pyproject-nix uv2nix pyproject-build-systems;
         };
 
+      in
+      rec {
+        packages.demoUvNixpkgsDerivation = demoUvNixpkgsDerivation;
+        packages.default = packages.demoUvNixpkgsDerivation;
 
-        # Registering as a package within the
-        # `apps` attribute set.
-        apps.${demoUvNixpkgsSystemd.pname} = self.apps.${system}.default;
-      });
+        devShells.default = pkgs.mkShell {
+          name = "demoUvNixpkgsDerivation-devshell";
+          inputsFrom = [ packages.demoUvNixpkgsDerivation ];
+          nativeBuildInputs = with pkgs; [ nixfmt-rfc-style ];
+        };
+      }
+    )
+    // flake-utils.lib.eachDefaultSystemPassThrough (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        systemdService = pkgs.callPackage ./nix/systemd-service.nix { inherit system; };
+      in
+      {
+        nixosModules.demoUvNixpkgs =
+          {
+            pkgs,
+            config,
+            lib,
+            ...
+          }:
+          let
+            cfg = config.services.demoUvNixpkgs;
+          in
+          {
+            options = with lib; {
+              services.demoUvNixpkgs = {
+                enable = mkEnableOption "Demo UV Nixpkgs";
+
+                package = mkOption {
+                  type = types.path;
+                  default = self.packages.${system}.default;
+                  description = "Package to use for UV Nixpkgs Demo service.";
+                };
+              };
+            };
+
+            config = lib.mkIf cfg.enable {
+              systemd.services.demoUvNixpkgs = {
+                wants = [ "network-online.target" ];
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                  Type = "simple";
+                  Restart = "always";
+                  DynamicUser = "yes";
+                  ExecStart = "${cfg.package}/bin/uv-nixpkgs";
+                };
+              };
+            };
+          };
+      }
+    );
 }
